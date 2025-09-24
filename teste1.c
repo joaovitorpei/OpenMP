520db53530dc04d0aba34fc44bfef9a4c97f04b3
Joao Vitor <joaovitorpei62@gmail.com>
2025-09-24 17:10:32 -0300

    novos teste

--- teste1.c
+++ teste1.c
@@ -3,7 +3,7 @@
 
 int main() {
     int a=1;
-    //omp_set_num_threads(2);
+    //omp_set_num_threads(2);//numero de threads serem ultilizadas nas regios paralelas
     #pragma omp parallel reduction(+:a)
     a=a+2;
     printf("resultado= %d\n",a);
